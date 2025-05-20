4b994864044643166eea813231e9f807723115a2
sahana-athota <80627030+sahana-athota@users.noreply.github.com>
2025-05-20 14:53:36 +0530

    basic backend for tasic1

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import mailRoutes from "./mails/route.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.DB_URL;
+//mongoose.connect(MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
+app.use(cors("*"));
+app.use(express.json());
+//const _dirname=dirname(fileURLToPath(import.meta.url));
+const _dirname="C:\\Users\\sahan\\Downloads\\Coding Week Task\\Coding Week Task"
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+app.use(express.static(path.join(_dirname, "frontend"))); 
+app.use("/api/mail", mailRoutes);
+
+app.get("/", async (req, res) => {
+   res.sendFile(_dirname+"/frontend/index.html");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ mails/controller.js
@@ -0,0 +1,53 @@
+import MailSchema from "./model.js";
+
+export const createMail = async (req, res) => {
+  const { sender, reciever, type, body } = req.body;
+  try {
+    const mail = new MailSchema({
+      sender,
+      reciever,
+      type,
+      body,
+    });
+    mail.save();
+    res.status(201).json(mail);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating mail", error });
+  }
+};
+
+export const getMails = async (req, res) => {
+  try {
+    const mails = await MailSchema.find();
+    res.status(200).json(mails);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteMail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Find the mail by ID and delete it
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting mail", error });
+  }
+};
+
+export const starMail = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Find the mail by ID and mark it as starred
+  } catch (error) {
+    res.status(500).json({ message: "Error starring mail", error });
+  }
+};
+
+const markAsRead = async (req, res) => {
+  const { id } = req.params;
+  try {
+    // Find the mail by ID and mark it as read
+  } catch (error) {
+    res.status(500).json({ message: "Error marking mail as read", error });
+  }
+};
